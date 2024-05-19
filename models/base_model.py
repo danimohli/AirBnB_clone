ac4ce4dd06d928b92e27e003cc6d0609a4ca8cd5
danimohli <danimohlife@gmail.com>
2024-05-19 14:06:51 +0100

    storage_added

--- models/base_model.py
+++ models/base_model.py
@@ -35,6 +35,7 @@ class BaseModel:
         the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON
+    file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path:
+        __file_path).
+        """
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects if the file exists.
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    cls_name = key.split('.')[0]
+                    cls = globals()[cls_name]
+                    self.__objects[key] = cls(**value)
+        except FileNotFoundError:
+            pass
