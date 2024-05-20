5baf7865ce509b3324ff5dcea450e549b04e53ac
danimohli <danimohlife@gmail.com>
2024-05-20 04:16:32 +0100

    create new file, update old ones

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -17,12 +22,14 @@ class FileStorage:
         """
         Returns the dictionary __objects.
         """
+
         return self.__objects
 
     def new(self, obj):
         """
-        Sets in __objects the obj with key <obj class name>.id.
+        Sets in __objects the obj with key <obj class name>.id
         """
+
         k = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[k] = obj
 
@@ -30,21 +37,22 @@ class FileStorage:
         """
         Serializes __objects to the JSON file (path: __file_path).
         """
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+
+        obj_dict = {k: obj.to_dict() for k, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects if the file exists.
+        Deserializes the JSON file to __objects (only if the JSON
+        file (__file_path) exists).
         """
+
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
-                for key, value in obj_dict.items():
-                    cls_name = key.split('.')[0]
-                    cls = globals().get(cls_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
+                for k, v in obj_dict.items():
+                    class_name = v['__class__']
+                    self.__objects[k] = eval(class_name)(**v)
         except FileNotFoundError:
             pass
