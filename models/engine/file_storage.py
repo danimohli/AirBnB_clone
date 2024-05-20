f5970ab8875fd1750fcb0b2dc7866de9cc63fb25
danimohli <danimohlife@gmail.com>
2024-05-20 04:33:29 +0100

    file_str update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,14 +22,12 @@ class FileStorage:
         """
         Returns the dictionary __objects.
         """
-
         return self.__objects
 
     def new(self, obj):
         """
         Sets in __objects the obj with key <obj class name>.id
         """
-
         k = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[k] = obj
 
@@ -37,22 +35,20 @@ class FileStorage:
         """
         Serializes __objects to the JSON file (path: __file_path).
         """
-
         obj_dict = {k: obj.to_dict() for k, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects (only if the JSON
-        file (__file_path) exists).
+        Deserializes the JSON file to __objects 
+        (only if the JSON file (__file_path) exists).
         """
-
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
                 for k, v in obj_dict.items():
                     class_name = v['__class__']
-                    self.__objects[k] = eval(class_name)(**v)
+                    self.__objects[k] = globals()[class_name](**v)
         except FileNotFoundError:
             pass
